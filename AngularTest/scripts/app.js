b8631d3fe2ba19a49ad5039943eb79e1f4b0b606
Tong Lin <brooklyntechlin@gmail.com>
2016-04-07 22:28:51 -0400

    added angular test

---
+++ AngularTest/scripts/app.js
@@ -0,0 +1,24 @@
+(function() {
+  var createWorker = function() {
+    var workCount = 0;
+
+    var task1 = function() {
+      workCount += 1;
+      console.log(`Task 1 + ${workCount}`);
+    };
+
+    var task2 = function() {
+      workCount += 2;
+      console.log("task2 " + workCount);
+    };
+
+    return {
+      job1: task1,
+      job2: task2
+    }
+  }
+
+  var worker = new createWorker();
+  worker.job1();
+  worker.job2();
+}());
