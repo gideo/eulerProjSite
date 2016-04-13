89a9b8d555403a09346916a523d6e8f4db623172
Tong Lin <brooklyntechlin@gmail.com>
2016-04-12 21:49:02 -0400

    removed comments

--- AngularTest/scripts/app.js
+++ AngularTest/scripts/app.js
@@ -1,33 +1,3 @@
-// (function() {
-//   var createWorker = function() {
-    
-//     var workCount = 0;
-
-//     var task1 = function() {
-//       workCount += 1;
-//       console.log(`Task 1 + ${workCount}`);
-//     };
-
-//     var task2 = function() {
-//       workCount += 2;
-//       console.log(`Task 2 + ${workCount}`);
-//     };
-
-//     return {
-//       job1: task1,
-//       job2: task2
-//     };
-//   };
-  
-//   var worker = new createWorker();
-//   worker.job1();
-//   worker.job2();
-// }());
-
-// var MainController = function($scope){
-//   $scope.message = "Hello!";
-// }
-
 var myApp = angular.module('myApp1',[]);
 
 myApp.controller('MainController', ['$scope', function($scope) {
