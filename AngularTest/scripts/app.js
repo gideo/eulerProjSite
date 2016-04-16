aae60139c108031c87f938b94fe20ea369c2391c
Tong Lin <brooklyntechlin@gmail.com>
2016-04-15 21:29:42 -0400

    updated test

--- AngularTest/scripts/app.js
+++ AngularTest/scripts/app.js
@@ -1,5 +1,25 @@
-var myApp = angular.module('myApp1',[]);
+// var myApp = angular.module('myApp1',[]);
 
-myApp.controller('MainController', ['$scope', function($scope) {
-  $scope.message = 'Hola!';
-}]);
+// myApp.controller('MainController', ['$scope', function($scope) {
+//   $scope.message = 'Hola!';
+// }]);
+(function(){
+  var app = angular.module('store', []);
+
+  app.controller('StoreController', function(){
+    this.product = gem;
+  });
+  
+  var gem = {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description: '. . .'
+  };
+  
+  var arr = {
+    a : ["A", "1", "Alpha"],
+    b : ["B", "2", "Beta"],
+    c : ["C", "3", "Gamma"]
+  };
+  
+})();
