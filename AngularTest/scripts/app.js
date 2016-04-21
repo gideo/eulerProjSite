914c9932af363217d9ee742a1289f98ed938849a
Tong Lin <brooklyntechlin@gmail.com>
2016-04-20 22:46:30 -0400

    inline css tests

--- AngularTest/scripts/app.js
+++ AngularTest/scripts/app.js
@@ -3,16 +3,16 @@
 
   app.controller('StoreController', function(){
     this.product = gem;
-    this.arrays = arr;
+    this.arrays1 = arr1;
   });
   
   var gem = {
     name: 'Dodecahedron',
     price: 2.95,
-    description: '. . .'
+    description: 'I am a dodecahedron, therefore I am dodecaliously twelve sided.'
   };
   
-  var arr = [
+  var arr1 = [
     {"num": "1", "letter": "Al"},
     {"num": "2", "letter": "Bee"},
     {"num": "3", "letter": "Gee"}
