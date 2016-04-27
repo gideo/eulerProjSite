8a15f7027d4ed15ca7bc516e5015aedf19877a6d
Tong Lin <brooklyntechlin@gmail.com>
2016-04-26 22:35:11 -0400

    testing spans

--- AngularTest/scripts/app.js
+++ AngularTest/scripts/app.js
@@ -1,12 +1,11 @@
 (function(){
   var app = angular.module('store', []);
-
+  
   app.controller('StoreController', function(){
     this.product = gems;
     this.arrays1 = arr1;
-    this.arrays2 = arr2;
+    this.books = book;
   });
-  
   var gems = {
     name: 'Dodecahedron',
     price: 2.95,
@@ -18,11 +17,13 @@
     {"num": "2", "letter": "Leather Jackets"},
     {"num": "3", "letter": "Into the Darkness"}
   ];
-
-  var arr2 = [
-    {"letter": "A", "desc": "The Riches"},
-    {"letter": "B", "desc": "Cahbs Secret"},
-    {"letter": "C", "desc": "Sinclair The World"}
-  ];
   
+  var book = [
+    {"title": "The Art of Animal Drawing", "author": " Ken Hultgren"},
+    {"title": "HTML & CSS : Design and Build Websites", "author": "Jon Duckett"},
+    {"title": "The C Programming Language", "author": "Brian W.Kerninghan, Dennis M.Ritchie"},
+    {"title": "A Song of Ice and Fire", "author": "George R.R. Martin"},
+    {"title": "Lord of the Rings", "author": "J.R.R. Tolkien"},
+    {"title": "Hyperion Cantos", "author": "Dan Simmons"}
+  ]
 })();
