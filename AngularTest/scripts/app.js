c700db40f53dac3d99e16eff1883882ceb792124
Tong Lin <brooklyntechlin@gmail.com>
2016-05-13 09:13:00 -0400

    removed comments

--- AngularTest/scripts/app.js
+++ AngularTest/scripts/app.js
@@ -7,15 +7,6 @@
     this.books = book;
     
     console.log("$http requesting");
-    
-    // $http({
-    //   method:'GET',
-    //   url: 'http://api.openweathermap.org/data/2.5/weather?q=Brooklyn,us&callback=test&appid=3bbd55527ee1ad0e5f326312904163d3',
-    // }).then(function(response) {
-    //   alert(response.data);
-    // }), function(reason) {
-    //   alert("No?");
-    // }
   });
   var gems = [ 
     {
