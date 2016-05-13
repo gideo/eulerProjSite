b46f894786bda487db90739b0fa02068c665bc2d
Tong Lin <brooklyntechlin@gmail.com>
2016-05-12 20:47:11 -0400

    not much

--- AngularTest/scripts/app.js
+++ AngularTest/scripts/app.js
@@ -5,23 +5,45 @@
     this.product = gems;
     this.arrays1 = arr1;
     this.books = book;
+    
+    console.log("$http requesting");
+    
+    // $http({
+    //   method:'GET',
+    //   url: 'http://api.openweathermap.org/data/2.5/weather?q=Brooklyn,us&callback=test&appid=3bbd55527ee1ad0e5f326312904163d3',
+    // }).then(function(response) {
+    //   alert(response.data);
+    // }), function(reason) {
+    //   alert("No?");
+    // }
   });
-  
-  var gems = [ {
-    "name": 'Dodecahedron',
-    "price": 2.95,
-    "description": 'I am a dodecahedron, therefore I am dodecaliously twelve sided.'},
-    {"name": 'Tetrahedron',
-    "price": 1.95,
-    "description": 'I am a tetrahedron, therefore I am not of this time.'
-  }
+  var gems = [ 
+    {
+      "name": 'Dodecahedron',
+      "price": 2.95,
+      "description": 'I am a dodecahedron, therefore I am dodecaliously twelve sided.'
+    },
+    {
+      "name": 'Tetrahedron',
+      "price": 1.95,
+      "description": 'I am a tetrahedron, therefore I am not of this time.'
+    },
+    {
+      "name": 'Pentagram',
+      "price": 5.00,
+      "description": 'I\'m pretty metal.'
+    }
   ];
   
   
   var arr1 = [
-    {"title": "In the Rose Garden", "letter": "Alabama Arkansas"},
-    {"title": "Falling down Grass", "letter": "Leather Jackets"},
-    {"title": "Unto the Breach", "letter": "Into the Darkness"}
+    {"title": "In the Rose Garden", "desc": "Alabama Arkansas"},
+    {"title": "Falling down Grass", "desc": "Leather Jackets"},
+    {"title": "Unto the Breach", "desc": "Into the Darkness"},
+    {"title": "Wysteria", "desc": "A Morsel of Food"},
+    {"title": "Wrathland", "desc": "Fomenting Dissent"},
+    {"title": "????"},
+    {"desc" : "?????"}
   ];
   
   var book = [
