817605c4c4fecdb3be247ffc434041ee37a84892
Tong Lin <brooklyntechlin@gmail.com>
2016-07-03 23:32:02 -0400

    removed functions

--- css_test/css/app.js
+++ css_test/css/app.js
@@ -1,18 +1,6 @@
 'use strict';
 
 $(document).ready(function() {
-  $(".paragraph").on("mouseover", function() {
-    $(this).addClass("active");
-  }).on("mouseleave", function() {
-    $(this).removeClass("active");
-  })
-  
-  $(".imgSize1").on("mouseover", function() {
-    $(this).addClass("squareL");
-  }).on("mouseleave", function() {
-    $(this).removeClass("squareL");
-  })
-  
   $(".title, .mainBox").on("mouseover", function() {
     $(this).addClass("squareL");
   }).on("mouseleave", function() {
