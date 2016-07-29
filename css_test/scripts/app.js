18f3d1344a5a74a303641bdca333446940ba534c
Tong Lin <brooklyntechlin@gmail.com>
2016-07-28 20:54:06 -0400

    removed test function

--- css_test/scripts/app.js
+++ css_test/scripts/app.js
@@ -1,15 +1,8 @@
 'use strict';
-
 $(document).ready(function() {
   $(".title, .mainBox").on("mouseover", function() {
     $(this).addClass("squareL");
-    
   }).on("mouseleave", function() {
     $(this).removeClass("squareL");
   });
-  
-  $("*").click(function() {
-    console.log(this);
-  });
-  
 })
