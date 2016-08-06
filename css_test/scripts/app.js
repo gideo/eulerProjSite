6716b2523165733093832dc8fa40fd3847319c48
Tong Lin <brooklyntechlin@gmail.com>
2016-08-05 20:55:50 -0400

    removed needless js

--- css_test/scripts/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-$(document).ready(function() {
-  $(".title, .mainBox").on("mouseover", function() {
-    $(this).addClass("squareL");
-  }).on("mouseleave", function() {
-    $(this).removeClass("squareL");
-  });
-})
