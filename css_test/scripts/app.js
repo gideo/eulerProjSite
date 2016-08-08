0142f67d55b42a0719bb2d6893f8ac60fcb3b854
Tong Lin <brooklyntechlin@gmail.com>
2016-08-07 20:14:24 -0400

    title border effect

--- css_test/scripts/app.js
+++ css_test/scripts/app.js
@@ -1,4 +1,8 @@
 'use strict';
 $(document).ready(function() {
-
+  $("#title, .mainBox").on("mouseover", function() {
+    $(this).addClass("titleL");
+  }).on("mouseleave", function() {
+    $(this).removeClass("titleL");
+  });
 })
