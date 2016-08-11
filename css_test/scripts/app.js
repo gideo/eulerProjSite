b7f5f7e4afaa1bad239a7cc03c255260865f10ba
Tong Lin <brooklyntechlin@gmail.com>
2016-08-10 21:03:08 -0400

    updated styles

--- css_test/scripts/app.js
+++ css_test/scripts/app.js
@@ -1,8 +1,14 @@
 'use strict';
 $(document).ready(function() {
-  $("#title, .paraBox").on("mouseover", function() {
+  $("#title").on("mouseover", function() {
+    $(this).addClass("borderM");
+  }).on("mouseleave", function() {
+    $(this).removeClass("borderM");
+  });
+  
+  $(".paraBox").on("mouseover", function() {
     $(this).addClass("borderL");
   }).on("mouseleave", function() {
     $(this).removeClass("borderL");
-  });
+  })
 })
