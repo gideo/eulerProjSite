b32e88c5b29170c7cb96104e2e650d2d10faf5f3
Tong Lin <brooklyntechlin@gmail.com>
2016-09-04 18:58:29 -0400

    changes to var

--- css_test/scripts/app.js
+++ css_test/scripts/app.js
@@ -1,4 +1,14 @@
 'use strict';
+
+let testFunction = function(str) {
+  document.getElementById(str).className += " test";
+  alert(d);
+}
+
+let testRemove = function(str) {
+  document.getElementById(str).className = document.getElementById(str).className.replace(/\btest\b/,'');
+}
+
 $(document).ready(function() {
   
   $("#title, .paraBox").on("mouseover", function() {
@@ -13,12 +23,3 @@ $(document).ready(function() {
     $(this).removeClass("borderB");
   });
 });
-
-let testFunction = function(str) {
-  document.getElementById(str).className += " test";
-  alert(d);
-}
-
-let testRemove = function(str) {
-  document.getElementById(str).className = document.getElementById(str).className.replace(/\btest\b/,'');
-}
