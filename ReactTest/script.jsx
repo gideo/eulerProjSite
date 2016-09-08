d055eb5d218a3ae3102a7f95b16702decc7427bd
Tong Lin <brooklyntechlin@gmail.com>
2016-09-08 15:59:08 -0400

    better button increment

--- ReactTest/script.jsx
+++ ReactTest/script.jsx
@@ -1,17 +1,43 @@
 // Code goes here
 
 var Button = React.createClass({
-  getInitialState: function() {
-    return {counter: 5};
-  },
-  handleClick: function() {
-    this.setState({ counter: this.state.counter+1 });
+  localHandleClick: function() {
+    this.props.localHandleClick(this.props.increment);  
   },
   render: function() {
     return (
-      <button onClick={this.handleClick}>{this.state.counter}</button>
+      <button onClick={this.localHandleClick}>+{this.props.increment}</button>
     )
   }
 });
 
-React.render(<Button />, document.getElementById("root"));
+var Result = React.createClass({
+  render: function() {
+    return (
+        <div>{this.props.localCounter}</div>
+      )
+  }
+});
+
+var Main = React.createClass({
+  getInitialState: function() {
+    return {counter: 0};
+  },
+  handleClick: function(increment) {
+    this.setState({ counter: this.state.counter+increment });
+  },
+  render: function() {
+    return (
+      <div>
+        <Button localHandleClick={this.handleClick} increment={1} />
+        <Button localHandleClick={this.handleClick} increment={5} />
+        <Button localHandleClick={this.handleClick} increment={10} />
+        <Button localHandleClick={this.handleClick} increment={100} />
+        <Result localCounter={this.state.counter} />
+      </div>
+    )
+  }
+})
+
+React.render(<Main />, document.getElementById("root"));
+
