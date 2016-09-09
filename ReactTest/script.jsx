1d854bfd1c0e2190f49b55f8912e891f5e1599c6
Tong Lin <brooklyntechlin@gmail.com>
2016-09-09 13:11:38 -0400

    added a multiplier

--- ReactTest/script.jsx
+++ ReactTest/script.jsx
@@ -11,6 +11,17 @@ var Button = React.createClass({
   }
 });
 
+var Double = React.createClass({
+    localHandleClick: function() {
+        this.props.localHandleClick(this.props.double);
+    },
+    render: function() {
+        return (
+            <button onClick={this.localHandleClick}>*{this.props.double}</button>
+        )
+    }
+});
+
 var Result = React.createClass({
   render: function() {
     return (
@@ -26,13 +37,19 @@ var Main = React.createClass({
   handleClick: function(increment) {
     this.setState({ counter: this.state.counter+increment });
   },
+  multClick: function(increment) {
+      this.setState( { counter: this.state.counter*increment });
+  },
   render: function() {
     return (
       <div>
         <Button localHandleClick={this.handleClick} increment={1} />
         <Button localHandleClick={this.handleClick} increment={5} />
         <Button localHandleClick={this.handleClick} increment={10} />
+        <Button localHandleClick={this.handleClick} increment={25} />
         <Button localHandleClick={this.handleClick} increment={100} />
+        <Button localHandleClick={this.handleClick} increment={1000} />
+        <Double localHandleClick={this.multClick} double={2} />
         <Result localCounter={this.state.counter} />
       </div>
     )
