0dbce20413260bfb03973280a6282b14afd06698
Tong Lin <brooklyntechlin@gmail.com>
2016-09-15 19:41:35 -0400

    jQuery + GitHub Api test

---
+++ ReactTest_GettingStarted/script.jsx
@@ -0,0 +1,34 @@
+// Code goes here
+
+var Card = React.createClass({
+    getInitialState: function() {
+        return {};  
+    },
+    componentDidMount: function() {
+        var component = this;
+        $.get("https://api.github.com/users/petehunt", function(data){
+          component.setState(data);
+        });
+    },
+    render: function() {
+        return (
+            <div> 
+                <img src={this.state.avatar_url} width="80" />
+                <h3>{this.state.name} + {this.state.email}</h3>
+                <hr/>
+            </div>
+        )
+    }
+});
+
+var Main = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <Card login="petehunt" />
+            </div>
+        )
+    }
+})
+
+React.render(<Main />, document.getElementById("root"));
