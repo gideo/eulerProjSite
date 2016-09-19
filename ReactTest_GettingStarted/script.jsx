6c32819b0ee82051a490b05c65d96be37c3ecdff
Tong Lin <brooklyntechlin@gmail.com>
2016-09-19 18:48:47 -0400

    removed email

--- ReactTest_GettingStarted/script.jsx
+++ ReactTest_GettingStarted/script.jsx
@@ -16,8 +16,7 @@ var Card = React.createClass({
                 <div className="userX"> 
                     <img src={this.state.avatar_url} width="80" />
                     <h3 className="fRight">{this.state.name}</h3>
-                    <h3>{this.state.email}</h3>
-                    <h3>{this.state.company} in {this.state.location}</h3>
+                    <h3>{this.state.company} In {this.state.location}</h3>
                 </div>
             </div>
         )
