8247bf4cd0af18aef657b1c13185e7680703ed81
Tong Lin <brooklyntechlin@gmail.com>
2016-09-21 18:35:26 -0400

    added input form

--- ReactTest_GettingStarted/script.jsx
+++ ReactTest_GettingStarted/script.jsx
@@ -1,3 +1,5 @@
+// Code goes here
+
 var Card = React.createClass({
     getInitialState: function() {
         return {};  
@@ -22,14 +24,38 @@ var Card = React.createClass({
     }
 });
 
-var Main = React.createClass({
+var Form = React.createClass({
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var loginInput = React.findDOMNode(this.refs.login);
+        this.props.addCard(loginInput.value);
+        loginInput.value = '';
+    },
     render: function() {
         return (
+            <form onSubmit={this.handleSubmit}>
+                <input placeholder="github login" ref="login"/>
+                <button>Add</button>
+            </form>
+        );
+    }
+})
+
+var Main = React.createClass({
+    getInitialState: function() {
+        return {logins: ['zpao', 'fisherwebdev', "petehunt", "spicyj"]};  
+    },
+    addCard: function(loginToAdd) {
+        this.setState({logins: this.state.logins.concat(loginToAdd)});
+    },
+    render: function() {
+        var cards = this.state.logins.map(function(login) {
+            return (<Card login={login} />);
+        });
+        return (
             <div>
-                <Card login="petehunt" />
-                <Card login="spicyj" />
-                <Card login="klkelley" />
-                <Card login="KevinMulhern" />
+                <Form addCard={this.addCard} />
+                {cards}
             </div>
         )
     }
