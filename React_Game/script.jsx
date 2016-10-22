4ac8591070dee384fe56a0cdfb1f8e31041ac47c
Tong Lin <brooklyntechlin@gmail.com>
2016-10-22 19:22:17 -0400

    added random to star amount

--- React_Game/script.jsx
+++ React_Game/script.jsx
@@ -2,14 +2,16 @@
 
 var StarsFrame = React.createClass({
     render: function() {
+        var numberOfStars = Math.floor(Math.random()*9);
+        
+        var stars = [];
+        for(var i = 0; i < numberOfStars; i++) {
+            stars.push(<span className="glyphicon glyphicon-star"></span>)
+        }
         return (
             <div id="stars-frame">
                 <div className="well">
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
-                    <span className="glyphicon glyphicon-star"></span>
+                    {stars};
                 </div>
             </div>
         )
