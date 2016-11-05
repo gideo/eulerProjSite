befb9a46fec593eeb328ed6c23b71c5302c28fd5
Tong Lin <brooklyntechlin@gmail.com>
2016-11-05 18:25:27 -0400

    spacing

--- React_Game/script.jsx
+++ React_Game/script.jsx
@@ -27,24 +27,26 @@ var ButtonFrame = React.createClass({
         switch(correct) {
             case true:
                 button = (
-                <button className="btn btn-success btn-lg" disabled={disabled}>
-                    <span className="glyphicon glyphicon-ok"></span>
-                </button>
+                    <button className="btn btn-success btn-lg" disabled={disabled}>
+                        <span className="glyphicon glyphicon-ok"></span>
+                    </button>
                 );
                 break;
+                
             case false:
                  button = (
-                <button className="btn btn-danger btn-lg" disabled={disabled}>
-                    <span className="glyphicon glyphicon-remove"></span>
-                </button>
+                    <button className="btn btn-danger btn-lg" disabled={disabled}>
+                        <span className="glyphicon glyphicon-remove"></span>
+                    </button>
                 );
                 break;
+                
             default:
                 disabled = (this.props.selectedNumbers.length === 0);
                 button = (
                     <button className="btn btn-primary btn-lg" disabled={disabled}
                             onClick={this.props.checkAnswer}>
-                        =
+                            =
                     </button>
                 );
         }
